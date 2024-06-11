bdc698da2f388e7bb17f5bfc8c3d8a3297693818
Anna <126054273+annymal@users.noreply.github.com>
2024-06-11 13:55:00 +0000

    update readmy

--- api.js
+++ api.js
@@ -1,34 +1,32 @@
-import axios from "axios"
-
+import axios from "axios";
 
 export async function sendMessage(text) {
-    try {
-        const response = await axios.post('http://localhost:3000/message',text)
-        return response.data
-    }
-    catch (error) {
-        console.error(error)
-    }
+  try {
+    const response = await axios.post("http://localhost:3000/message", text);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-const img = document.querySelector('.section__content-chat img')
-const span = document.querySelector('.section__content-chat span')
-const chatContainer = document.querySelector('.section__content-chat')
+const img = document.querySelector(".section__content-chat img");
+const span = document.querySelector(".section__content-chat span");
+const chatContainer = document.querySelector(".section__content-chat");
 
 export async function getMessage() {
-    try {
-        const response = await axios.get('http://localhost:3000/message')
-        img.style.display = 'none'
-        span.style.display = 'none'
+  try {
+    const response = await axios.get("http://localhost:3000/message");
+    img.style.display = "none";
+    span.style.display = "none";
+    chatContainer.style.alignItems = "end";
 
-        response.data.forEach((item) => {
-        const messageElement = document.createElement('div')
-        messageElement.classList.add('message')
-        messageElement.textContent = item.value
-        chatContainer.appendChild(messageElement)
-        })
-    }
-    catch (e) {
-        console.log(e)
-    }
+    response.data.forEach((item) => {
+      const messageElement = document.createElement("div");
+      messageElement.classList.add("message");
+      messageElement.textContent = item.value;
+      chatContainer.appendChild(messageElement);
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
