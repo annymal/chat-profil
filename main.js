62c7ed9ea21c38075c0784cdc8c52e33674e99b2
Anna <126054273+annymal@users.noreply.github.com>
2024-06-11 15:10:21 +0000

    refacror: change style

--- main.js
+++ main.js
@@ -1,32 +1,27 @@
-import { getMessage, sendMessage } from "./api"
+import { getMessage, sendMessage } from "./api";
 
-const textarea = document.getElementById('textarea')
-const btn =  document.getElementById('btn-send')
+const textarea = document.getElementById("textarea");
+const btn = document.getElementById("btn-send");
 
- btn.addEventListener('click', handleKeyPress)
-    
- textarea.addEventListener('keydown', handleKeyPress)
+btn.addEventListener("click", handleKeyPress);
 
- async function handleKeyPress(event) {
-    const {value} = textarea
-    if (event.type == 'click' || event.key === 'Enter') {
-      event.preventDefault();
-      if (textarea.value.trim() === '') {
-         btn.classList.add('disabled')
-         return
-      }
-      btn.classList.remove('disabled')
-        try {
-            await sendMessage({value})
-            await getMessage()
-            textarea.value = ''
-         
-         }
-         catch(error) {
-            console.error('Ошибка при отправке сообщения', error)
-         }
+textarea.addEventListener("keydown", handleKeyPress);
+
+async function handleKeyPress(event) {
+  const { value } = textarea;
+  if (event.type == "click" || event.key === "Enter") {
+    event.preventDefault();
+    if (textarea.value.trim() === "") {
+      btn.classList.add("disabled");
+      return;
     }
- }
-
-
-
+    btn.classList.remove("disabled");
+    try {
+      await sendMessage({ value });
+      await getMessage();
+      textarea.value = "";
+    } catch (error) {
+      console.error("Ошибка при отправке сообщения", error);
+    }
+  }
+}
